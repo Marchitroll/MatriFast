9758212329515c1c64a1bfc6b3b0740dc0bee369
Carlo312003 <117953272+Carl312003@users.noreply.github.com>
2025-07-06 09:51:29 -0500

    Update Perfil.jsx

--- frontend/src/paginas/Perfil.jsx
+++ frontend/src/paginas/Perfil.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../funcionalidad/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import DocenteFields from '../componentes/DocenteFields';
